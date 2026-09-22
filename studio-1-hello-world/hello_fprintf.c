4c47e5d180107dab11038d281df02e35bc98008b
Michelle schepers <michelle.schepers@slu.edu>
2026-09-22 13:48:17 -0500

    C files
    exit
    git push

--- studio-1-hello-world/hello_fprintf.c
+++ studio-1-hello-world/hello_fprintf.c
@@ -1,1 +1,9 @@
+// michelle schepers // sep 22 // hello world 
+#include <stdio.h>
+
+int main( int argc, char* argv[] ){ //Make a call to fprintf() here return 0; }`
+  fprintf(stdout, "Hello, world!\n");
+  return 0; 
+  }
+
 //Replace this file with the contents of your hello_fprintf.c
--- studio-1-hello-world/hello_write.c
+++ studio-1-hello-world/hello_write.c
@@ -1,1 +1,9 @@
+// michelle schepers // sep 22 // hello world 
+#include <unistd.h>
+
+int main( int argc, char* argv[] ){ //
+  write(STDOUT_FILENO, "Hello, world!\n", 1000);
+  return 0; 
+  }
+
 //Replace the contents of this file with your hello_write.c
